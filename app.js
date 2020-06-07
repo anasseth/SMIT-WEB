54e5fc5f5d3179f0dac076e04a6db60d2dcc2939
anas <m.anasseth00@gmail.com>
2020-06-07 15:53:50 +0500

    'assignment-5-completed'

---
+++ app.js
@@ -0,0 +1,63 @@
+
+
+alert(' Hi User !!! ')
+
+
+alert(' Error! Please Enter A Valid Password ')
+
+
+alert(' Welcome To JS Land.... \n Happy Coding! ')
+
+
+alert(" Hello I can run JS through my web browser's console ")
+
+
+// < html >
+
+//     <head> 
+//         {/* Head */}
+//         <script>alert(' Hi User !!! ')</script>
+//     </head>
+    
+//     {/* Body (before your page’s HTML)  */}
+//     <script>alert(' Hi User !!! ')</script>
+    
+//     <body>
+    
+//     {/* Body (inside your page’s HTML) */}
+//     <script>alert(' Hi User !!! ')</script>
+
+//     <h1> Hello World With JavaScript </h1>
+        
+//     {/* Body (after your page’s HTML)     */}
+//     <script>alert(' Hi User !!! ')</script>  
+    
+//     </body>
+
+// </html>
+
+var FirstName = 'Muhammad';
+var LastName = 'Anas'
+var FullName = FirstName + LastName;
+
+var message = 'Hello World'
+alert(message)
+
+var StuName = 'Jhon Doe'
+var StuAge = '15 years old'
+var StuCourse = 'Certified Mobile Application Development'
+alert(StuName)
+alert(StuAge)
+alert(StuCourse)
+
+var Pizza = ' PIZZA \n PIZZ \n PIZ \n PI \n P '
+alert(Pizza)
+
+var myEmail= 'm.anasseth00@gmail.com'
+alert('my email address is '+ email)
+
+var book = 'A Smarter Way To Learn JavaScript'
+alert('I am Trying to learn from the book '+ book )
+
+document.getElementsByTagName('body').innerHTML = '<h1>Yah ! I can write HTML content through JavaScript</h1>'
+
