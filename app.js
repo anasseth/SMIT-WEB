9137736290dc3728d2e51cd1844b56b57b0936a2
anas <m.anasseth00@gmail.com>
2020-06-07 16:29:03 +0500

    'assignment-5-completed'

--- app.js
+++ app.js
@@ -61,3 +61,41 @@ alert('I am Trying to learn from the book '+ book )
 
 document.getElementsByTagName('body').innerHTML = '<h1>Yah ! I can write HTML content through JavaScript</h1>'
 
+
+var myage = 'I am 15 years old'
+alert(myage)
+
+var visitorsNumber = 14 
+alert('you have visited this site '+visitorsNumber+' times.')
+
+var birthYear = 1990;
+alert('My Birth Year is '+birthYear+'\n Data type of my birth year is number') 
+
+var vizName = 'John Doe'
+var vizProduct = 'T-Shirts'
+var vizQuantity = 5
+alert(vizName+' ordered '+vizQuantity+' '+vizProduct+'(s) on XYZ Clothing Store')
+
+
+
+
+var a=1, b=2,c=3 ;
+
+// var 1stNumber= 'Error !! Illegal Variable Name';
+// var .iop = 'Error !! Illegal Variable Name';
+// var -ofdfo= 'Error !! Illegal Variable Name';
+// var @ffjdfsd = 'Error !! Illegal Variable Name';
+// var %$#@* = 'Error !! Illegal Variable Name';
+
+var a1stNumber = 'Super !! legal Variable Name'
+var iop = 'Super !! legal Variable Name';
+var _ofdfo ='Super !! legal Variable Name';
+var ffjdsd = 'Super !! legal Variable Name';
+var $OnlyDollarSignAllowed = 'Super !! legal Variable Name';
+
+var rulesForNamingJavascriptVariable = '<h2>Rules for naming JS variables</h2> <br /> <p> Variable names can only contain numbers, $ and __ .</p> <br /> <p> Variables must begin with a dollar sign $ , underscore or letter . For example $name, _name or name</p> <br /> <p>  Variable names are case-Sensitive</p> <br /> <p>Variable names should not be JS KeyWords</p> '
+document.getElementsByTagName('body').innerHTML = rulesForNamingJavascriptVariable
+
+
+
+
